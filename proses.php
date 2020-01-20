7e80a2d121d594136aa491be519c0a7885e39911
Budi Julian <45169749+budijulian@users.noreply.github.com>
2020-01-21 00:28:00 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,6 +1,5 @@
-<?php
-//cek button    
-if ($_POST['Submit'] == "Submit") {
+<?php   
+
     $id    = $_POST['id_mahasiswa'];
     $nama            = $_POST['nama'];
     $jurusan        = $_POST['jurusan'];
@@ -9,11 +8,10 @@ if ($_POST['Submit'] == "Submit") {
 
 include 'koneksi.php';
 // query SQL untuk insert data
-$query="INSERT INTO db_mahasiswa SET ID='$id',NAMA='$nama',JURUSAN='$jurusan',ALAMAT='$alamat',NOHP='$telepon'";
+$query="INSERT INTO db_mahasiswa VALUES ('$id','$nama','$jurusan','$alamat','$telepon')";
 sqlsrv_query($conn, $query);
 // mengalihkan ke halaman index.php
 header("location:index.php");
 
-    }
-    
+   
 ?>
