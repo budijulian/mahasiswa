bf1e50bec230e5d6d1dab8449403fe27d26f506e
Budi Julian <45169749+budijulian@users.noreply.github.com>
2020-01-20 23:02:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,63 @@
+<!DOCTYPE html>
 <html>
+
 <head>
-    <title>DATA MAHASISWA - BUDIJULIAN</title>
-    <style type="text/css" media="screen">
-        table {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11px;}
-        input {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11px;height: 20px;}
-    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Data Mahasiswa | Tambah Data</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
+
 <body>
-<div style="border:0; padding:10px; margin-left:50px; width:760px; height:auto;">
-<form action="action-input-data.php" method="POST" name="form-input-data">
-    <table width="760" border="0" align="center" cellpadding="0" cellspacing="0">
-        <tr height="46">
-                <td width="10%"> </td>
-                <td width="25%"> </td>
-                <td width="65%"><font color="orange" size="2">FORM TAMBAH DATA MAHASISWA</font></td>
-        </tr>
-        <tr height="46">
-            <td> </td>
-            <td>ID Mahasiswa / NIM</td>
-            <td><input type="text" name="id_mahasiswa" size="35" maxlength="6" /></td>
-        </tr>
-        <tr height="46">
-            <td> </td>
-            <td>Nama</td>
-            <td><input type="text" name="nama" size="50" maxlength="30" /></td>
-        </tr>
-        <tr height="46">
-            <td> </td>
-            <td>Jurusan</td>
-            <td><select name="jurusan">
-                    <option value="-">- Pilih Jurusan -
-                    <option value="Teknik Komputer">Teknik Komputer
-                    <option value="Teknik Informatika">Teknik Informatika
-                    <option value="Teknik Mesin">Teknik Mesin
-                    <option value="Teknik Elektro">Teknik Elektro
-                    <option value="Komputer Akuntansi">Komputer Akuntansi
-                </select></td>
-        </tr>
-        <tr height="46">
-            <td> </td>
-            <td>Alamat</td>
-            <td><input type="text" name="alamat" size="50" maxlength="30" /></td>
-        </tr>
-        <tr height="46">
-            <td> </td>
-            <td>No. Telp</td>
-            <td><input type="text" name="telepon" size="20" maxlength="12" /></td>
-        </tr>
-        <tr height="46">
-            <td> </td>
-            <td> </td>
-            <td><input type="submit" name="Submit" value="Submit">   
-                <input type="reset" name="reset" value="Cancel"></td>
-        </tr>
-    </table>
-</form>
-</div>
+    <div class="container mt-5">
+        <form action="proses.php" method="POST">
+            <div class="col col-sm-12">
+                <div class="form-group">
+                        <h2 class="text-success text-center">FORM TAMBAH DATA MAHASISWA</h2>
+                </div>
+                <div class="form-group">
+                    <td>ID Mahasiswa / NIM</td>
+                    <td><input class="form-control" type="text" name="id_mahasiswa" size="35" maxlength="6" /></td>
+                </div>
+                <div class="form-group">
+
+                    <td>Nama</td>
+                    <td><input class="form-control" type="text" name="nama" size="50" maxlength="30" /></td>
+                </div>
+                <div class="form-group">
+
+                    <td>Jurusan</td>
+                    <td><select class="form-control" name="jurusan">
+                            <option value="-">- Pilih Jurusan -
+                            <option value="Teknik Komputer">Teknik Komputer
+                            <option value="Teknik Informatika">Teknik Informatika
+                            <option value="Teknik Mesin">Teknik Mesin
+                            <option value="Teknik Elektro">Teknik Elektro
+                            <option value="Komputer Akuntansi">Komputer Akuntansi
+                        </select></td>
+                </div>
+                <div class="form-group">
+                    <td> </td>
+                    <td>Alamat</td>
+                    <td><input class="form-control" type="text" name="alamat" size="50" maxlength="30" /></td>
+                </div>
+                <div class="form-group">
+                    <td> </td>
+                    <td>No. Telp</td>
+                    <td><input class="form-control" type="text" name="telepon" size="20" maxlength="12" /></td>
+                </div>
+                <div class="form-group">
+                    <td> </td>
+                    <td> </td>
+                    <td><input class="btn btn-primary" type="submit" type="button" name="Submit" value="Submit">
+                        <input class="btn btn-danger" type="reset" type="button" name="reset" value="Cancel"></td>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
