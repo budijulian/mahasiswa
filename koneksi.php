1b34eb130a991ea554f6c7613bfd9c775674cb00
Budi Julian <45169749+budijulian@users.noreply.github.com>
2020-01-21 00:16:35 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,16 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:mhsazureserver.database.windows.net,1433; Database = db_mahasiswa", "budijulian", "24Budi24");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// try {
+//     $conn = new PDO("sqlsrv:server = tcp:mhsazureserver.database.windows.net,1433; Database = db_mahasiswa", "budijulian", "24Budi24");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// }
+// catch (PDOException $e) {
+//     print("Error connecting to SQL Server.");
+//     die(print_r($e));
+// }
 
 // SQL Server Extension Sample Code:
-// $connectionInfo = array("UID" => "budijulian", "pwd" => "24Budi24", "Database" => "db_mahasiswa", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-// $serverName = "tcp:mhsazureserver.database.windows.net,1433";
-// $conn = sqlsrv_connect($serverName, $connectionInfo);
+$connectionInfo = array("UID" => "budijulian", "pwd" => "24Budi24", "Database" => "db_mahasiswa", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mhsazureserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
