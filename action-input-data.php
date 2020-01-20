4f6794609d50a4c94faa2e70a146e82c90b01c1f
Budi Julian <45169749+budijulian@users.noreply.github.com>
2020-01-20 19:59:11 +0700

    Add files via upload

---
+++ action-input-data.php
@@ -0,0 +1,19 @@
+<?php
+//cek button    
+    if ($_POST['Submit'] == "Submit") {
+    $id_mahasiswa    = $_POST['id_mahasiswa'];
+    $nama            = $_POST['nama'];
+    $jurusan        = $_POST['jurusan'];
+    $alamat            = $_POST['alamat'];
+    $telepon        = $_POST['telepon'];
+
+include 'koneksi.php';
+// query SQL untuk insert data
+$query="INSERT INTO mahasiswa SET id_mahasiswa='$nim',nama='$nama',jurusan='$jurusan',alamat='$alamat',telepon='$telepon'";
+mysqli_query($koneksi, $query);
+// mengalihkan ke halaman index.php
+header("location:index.php");
+
+    }
+    
+?>
