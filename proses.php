f865d8c93fa57e0c845e4e5bf8d2941fab28313e
Budi Julian <45169749+budijulian@users.noreply.github.com>
2020-01-20 23:01:36 +0700

    Update and rename action-input-data.php to proses.php

--- proses.php
+++ proses.php
@@ -1,10 +1,10 @@
 <?php
 //cek button    
-    if ($_POST['Submit'] == "Submit") {
+if ($_POST['Submit'] == "Submit") {
     $id_mahasiswa    = $_POST['id_mahasiswa'];
     $nama            = $_POST['nama'];
     $jurusan        = $_POST['jurusan'];
-    $alamat            = $_POST['alamat'];
+    $alamat          = $_POST['alamat'];
     $telepon        = $_POST['telepon'];
 
 include 'koneksi.php';
